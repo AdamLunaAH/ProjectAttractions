b0ddf9f99ffb8e0d28ecf197bcd6a74e408d4303
Adam <161696943+AdamLunaAH@users.noreply.github.com>
2025-09-03 14:33:51 +0200

    first

---
+++ DbModels/CreditCardDbM.cs
@@ -0,0 +1,17 @@
+using Seido.Utilities.SeedGenerator;
+using Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbModels;
+
+public class CreditCardDbM : CreditCard, ISeed<CreditCardDbM>
+{
+    [Key]
+    public override Guid CreditCardId { get; set; }
+
+    public new CreditCardDbM Seed(SeedGenerator seeder)
+    {
+        base.Seed(seeder);
+        return this;
+    }
+}
